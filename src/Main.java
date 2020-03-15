2b2a2e50dbf2f352b85098a0b8b4b2d8be73244a
psantos-stack <54780578+psantos-stack@users.noreply.github.com>
2020-03-14 23:56:14 -0700

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
 public class Main {
 
     public static void main(String[] args) {
@@ -14,5 +16,28 @@ public class Main {
         System.out.println(a.toString());
         //a.pop();
         //System.out.println(a.toString());
+    }
+    @Test
+    public void add(){
+        MaxHeap a = new MaxHeap(3);
+        for(int i = 0; i < 3; i++){
+            a.add(i);
+        }
+        Integer[] expected = {2, 0, 1};
+        for(int i = 0; i < 3; i++){
+            assertEquals(a.data[i], expected[i]);
+        }
+    }
+    @Test
+    public void pop(){
+        MaxHeap a = new MaxHeap(3);
+        for(int i = 0; i < 3; i++){
+            a.add(i);
+        }
+        assertEquals(2, a.pop().intValue());
+        Integer[] expected = {1, 0};
+        for(int i = 0; i < 2; i++){
+            assertEquals(a.data[i], expected[i]);
+        }
     }
 }
